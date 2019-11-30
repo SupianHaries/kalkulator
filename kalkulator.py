c4bafa6550257ad4420681a60e69c22aff7cb696
SupianHaries <58073587+SupianHaries@users.noreply.github.com>
2019-11-30 18:33:28 +0800

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,47 @@
+df =' ____  _   _ ___ ____ _____ ' 
+gf ='/ ___|| | | |_ _|  _ \___  |'
+ef ='\___ \| |_| || || | | | / / ' 
+td =' ___) |  _  || || |_| |/ /  '  
+bn ='|____/|_| |_|___|____//_/   '
+print(df) ; print(gf) ; print(ef) ; print(td) ; print(bn) ;
+print('=======>>>')
+kl = '   Coded By SupianHaris_ID'
+kp = '     Kalkulator V.01'
+print(kl) ; print(kp) ;
+print('=======>>>')
+print('~> Pertambahan')
+x = input('Input = ')
+x += input('Input = ')
+print('        ---- (+)')
+print(' ~>',x)  
+print('===========')
+print('~> Pengurangan')
+x = input('Input = ')
+x -= input('Input = ')
+print('        ---- (-)')
+print(' ~>',x) 
+print('===========')
+print('~> Perkalian')
+x = input('Input = ')
+x *= input('Input = ')
+print('        ---- (x)')
+print(' ~>',x)
+print('===========')
+print('~> Pembagian')
+x = input('Input = ')
+x /= input('Input = ')
+print('        ---- (:)')
+print(' ~>',x)
+print('===========')
+print('~> Pembagian Bulat')
+x = input('Input = ')
+x //= input('Input = ')
+print('        ---- ')
+print(' ~>', x)
+print('===========')
+print('~> Berpangkat')
+x = input('Input = ')
+x **= input('Input = ')
+print('        ---- ')
+print(' ~> ', x)
+print('===========')
